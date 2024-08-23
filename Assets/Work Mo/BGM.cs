6d76f9dde9074c64121bc6f2aedf4fdb64295603
masakan590 <113155308+masakan590@users.noreply.github.com>
2024-08-23 11:30:10 +0900

    いろいろ変更

---
+++ Assets/Work Mo/BGM.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip bgmStage1;
+    public AudioClip bgmStage2;
+    public AudioClip bgmStage3;
+
+    private string currentSceneName;
+
+    private void Start()
+    {
+        currentSceneName = SceneManager.GetActiveScene().name;
+        PlayBGM(currentSceneName);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string newSceneName = scene.name;
+
+        if (newSceneName != currentSceneName)
+        {
+            currentSceneName = newSceneName;
+            PlayBGM(currentSceneName);
+        }
+    }
+
+    private void PlayBGM(string sceneName)
+    {
+        switch (sceneName)
+        {
+            case "Stage_1-1":
+                audioSource.clip = bgmStage1;
+                break;
+            case "Stage1-2":
+                audioSource.clip = bgmStage2;
+                break;
+            case "Stage1-3":
+                audioSource.clip = bgmStage3;
+                break;
+            default:
+                audioSource.clip = null;
+                break;
+        }
+
+        if (audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+    }
+}

85d9b661b5232ec5c1a37ab0922fdd73bdc65f5a
nozomu1210 <113155310+nozomu1210@users.noreply.github.com>
2024-08-30 11:48:26 +0900

    escでタイトル

--- Assets/Work Nu/Script/Esc.cs
+++ Assets/Work Nu/Script/Esc.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Esc : MonoBehaviour
 {
-    // 遷移先のシーン名を指定
+    
     private string scene = "TitleScene";
 
     void Update()
     {
-        // エスケープキーが押されたかをチェック
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // シーンをロード
+            
             SceneManager.LoadScene(scene);
         }
     }
